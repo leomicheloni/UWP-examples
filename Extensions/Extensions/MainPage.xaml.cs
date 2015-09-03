50f5048d5bd6d6e048b999bd7cf812aa956b931a
Leonardo <leomicheloni@hotmail.com>
2015-09-02 23:21:41 -0300

    extensions example

---
+++ Extensions/Extensions/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Extensions
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            if(Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+                Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        private void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void button_Click(System.Object sender, RoutedEventArgs e)
+        {
+            App.Current.Exit();
+        }
+    }
+}
